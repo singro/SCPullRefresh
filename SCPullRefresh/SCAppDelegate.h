a31f2cd2da52fcf2288c4ccf470bba2634dbae40
singro <isingro@gmail.com>
2014-08-13 23:34:40 +0800

    Init project.

---
+++ SCPullRefresh/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  SCPullRefresh
+//
+//  Created by Singro on 8/13/14.
+//  Copyright (c) 2014 Singro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
